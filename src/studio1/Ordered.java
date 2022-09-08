e824da5673f53e3012f96384711d7ef897d6fde5
andresorbegozo <andresorbegozo@localhost>
2022-09-08 12:36:47 -0500

    Studio1 Complete!

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,13 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		boolean ordered1 = x-y > 0;
+		boolean ordered2 = x-z > 0;
+		boolean ordered3 = z-y > 0;
+		boolean ordered4 = z-x > 0;
+		boolean isOrdered = (ordered1 && ordered2) || (ordered3 && ordered4);
+		System.out.println(isOrdered);
+		
 
 	}
 
